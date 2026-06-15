fff3fa80105b20358b5fc7206a0bf522207d0274
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-15 10:40:02 +0530

    Add: task-scheduling - submission-7

---
+++ Data Structures & Algorithms/task-scheduling/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        newTasks = [[0, chr(i+ord('A'))] for i in range(26)]
+        for i in range(len(tasks)):
+            newTasks[ord(tasks[i])-ord('A')][0] -= 1
+        heapq.heapify(newTasks)
+        cooldown = collections.deque()
+        tasksLeft = len(tasks)
+        i = 0
+        while tasksLeft != 0:
+            if not cooldown and newTasks[0][0] == 0:
+                break
+            #cooldown[i][char, remaining occurences, timeWhenFree]
+            if cooldown and cooldown[0][2] == i:
+                char, freq, cd = cooldown.popleft()
+                heapq.heappush(newTasks, [freq, char])
+            i+=1
+            if newTasks and newTasks[0][0] != 0:
+                remainingFreq, char = heapq.heappop(newTasks)
+                if remainingFreq<-1:
+                    cooldown.append([char, remainingFreq+1, i+n])
+                tasksLeft -= 1
+
+        return i
