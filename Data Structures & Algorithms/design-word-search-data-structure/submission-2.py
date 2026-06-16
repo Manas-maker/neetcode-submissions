cc866ffba6623c6a270bc003b33946614f66575a
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-17 02:34:32 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,28 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+            else:
+                cur.children[c] = TrieNode()
+                cur = cur.children[c]
+        cur.end = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        def searchRecurse(word, cur):
+            if not word and cur.end: return True
+            if not word or  word[0]!='.' and word[0] not in cur.children: return False
+            if word[0]=='.':
+                return any(searchRecurse(word[1:], cur.children[c]) for c in cur.children)
+            elif word[0] in cur.children: return searchRecurse(word[1:], cur.children[word[0]])
+        return searchRecurse(word, cur)
