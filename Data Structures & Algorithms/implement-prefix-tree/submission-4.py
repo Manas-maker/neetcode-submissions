687b88e026391ccb1f8ea82e569a9fd5e52ad6d3
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-16 09:44:23 +0530

    Add: implement-prefix-tree - submission-4

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-4.py
@@ -0,0 +1,29 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root = {'charMap': {}, 'end':False}
+        for i in range(26):
+            self.root[chr(i+ord('a'))] = None
+
+    def insert(self, word: str) -> None:
+        if (len(word)==0): self.root['end']=True
+        if len(word)>0:
+            nextNode = PrefixTree() if word[0] not in self.root['charMap'] else self.root['charMap'][word[0]]
+            self.root['charMap'][word[0]] = nextNode
+            nextNode.insert(word[1:])
+
+    def search(self, word: str) -> bool:
+        if not word and self.root['end']:
+            return True
+        elif len(word)>0  and word[0] in self.root['charMap']:
+            return self.root['charMap'][word[0]].search(word[1:])
+        else:
+            return False
+
+    def startsWith(self, prefix: str) -> bool:
+        if not prefix:
+            return True
+        elif prefix and prefix[0] in self.root['charMap']:
+            return self.root['charMap'][prefix[0]].startsWith(prefix[1:])
+        else:
+            return False
