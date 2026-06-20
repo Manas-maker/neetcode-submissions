f98389fbc6fb016056c666e22413c7e73871e89d
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-20 23:42:37 +0530

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def dfs(i, s, openCount, closedCount):
+            if i==closedCount and openCount==0:
+                res.append(s)
+                return
+            if openCount>0:
+                dfs(i, s+')', openCount-1, closedCount+1)
+            if closedCount<i and (openCount+closedCount)<=i:
+                dfs(i, s+'(', openCount+1, closedCount)
+        dfs(n, '', 0, 0)
+        return res
+            
