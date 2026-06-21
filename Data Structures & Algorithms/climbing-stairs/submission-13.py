37bc22518bd97e636a12e9855b1544c63d6e8b5b
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-21 17:42:49 +0530

    Add: climbing-stairs - submission-13

---
+++ Data Structures & Algorithms/climbing-stairs/submission-13.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2: return n
+        saves = [0]*(n+1)
+        saves[1], saves[2] = 1, 2
+        print(saves)
+        for i in range(3, n+1):
+            saves[i] = saves[i-1] + saves[i-2]
+        return saves[n]
