38266df89c7d58273c68fa54fca0e3ad28f87f99
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-21 17:33:09 +0530

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        res = 0
+        saves = {-1: 0, 0: 0, 1: 1, 2: 2}
+        def dfs(n):
+            if n in saves: return saves[n]
+            if n>=3:
+                saves[n] = dfs(n-1)+dfs(n-2)
+            return saves[n]
+        dfs(n)
+        return saves[n]
