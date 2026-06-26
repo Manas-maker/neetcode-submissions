6ae082dc1cc3399d7cc18eb7491d83e03de223a9
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-26 16:49:47 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return None
+        def bfc(oldNode):
+            seen = {}
+            cur = oldNode
+            newcur = newRoot = Node()
+            newcur.val = cur.val
+            seen[cur]=newcur
+            q = collections.deque()
+            q.append((cur, newcur))
+            while q:
+                cur, newcur = q.popleft()
+                if not cur.neighbors: continue
+                for neighbor in cur.neighbors:
+                    newNeighbor = seen.get(neighbor, Node(neighbor.val))
+                    newcur.neighbors.append(newNeighbor)
+                    if neighbor not in seen:
+                        q.append((neighbor, newNeighbor))
+                        seen[neighbor] = newNeighbor
+            return newRoot
+        return bfc(node)
