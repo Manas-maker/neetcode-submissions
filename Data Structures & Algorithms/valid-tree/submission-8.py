0a0ee2b2dbbcfb591fb33c392d15df58ba9484f2
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-06-29 15:36:04 +0530

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.py
@@ -0,0 +1,19 @@
+#Iterative solution
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        stack = [(None, 0)]
+        seen = set()
+        if len(edges)!=n-1: return False
+        mappings = [[] for _ in range(n)]
+        for a, b in edges:
+            mappings[a].append(b)
+            mappings[b].append(a)
+        prev = cur = None
+        while stack:
+            prev, cur = stack.pop()
+            if cur in seen: return False
+            seen.add(cur)
+            for nei in mappings[cur]:
+                if nei!=prev:
+                    stack.append([cur, nei])
+        return len(seen)==n
