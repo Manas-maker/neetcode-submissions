889394c33c14f9d3fffdd94a9d4b99e6d1c90ef9
Manas Pradhan <64654573+Manas-maker@users.noreply.github.com>
2026-07-08 11:37:38 +0530

    Add: hand-of-straights - submission-5

---
+++ Data Structures & Algorithms/hand-of-straights/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand)%groupSize!=0: return False
+        hand.sort()
+        counts = {}
+        i=0
+        maxCard = 0
+        for k in hand:
+            if k not in counts:
+                counts[k]=1
+                maxCard = max(maxCard, k)
+            else: counts[k] += 1
+        for i in hand:
+            if counts[i]>0:
+                freq = counts[i]
+                for j in range(groupSize):
+                    if (i+j) not in counts: return False
+                    counts[i+j]-=freq
+                    if counts[i+j]<0: return False
+        return all(counts[i]==0 for i in counts)
